054d4c48c9e144c616d28a54d201ec9feefec6bb
sank0118 <tks9901@naver.com>
2025-02-14 10:33:47 +0900

    폼 만듦

---
+++ src/Cmsform.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const Cmsform = () => {
+  const [first, setfirst] = useState(second);
+
+  const onChange = (e) => {};
+
+  const onSubmit = (e) => {};
+
+  return (
+    <form>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" />
+      </div>
+      <button></button>
+    </form>
+  );
+};
+
+export default Cmsform;
